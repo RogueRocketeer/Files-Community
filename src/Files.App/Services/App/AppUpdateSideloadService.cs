b31d58a1890adc701fb250942c84769f7fdf7ffe
Yair <39923744+yaira2@users.noreply.github.com>
2025-03-23 16:21:37 -0400

    Update AppUpdateSideloadService.cs

--- src/Files.App/Services/App/AppUpdateSideloadService.cs
+++ src/Files.App/Services/App/AppUpdateSideloadService.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Files Community
 // Licensed under the MIT License.
 
-using CommunityToolkit.WinUI.Helpers;
 using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Net.Http;
